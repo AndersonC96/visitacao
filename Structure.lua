79b5ba0529f6aca81143343111a76623bc4f9428
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-07-16 12:17:53 -0300

    .

---
+++ Structure.lua
@@ -0,0 +1,26 @@
+projeto/
+│
+├── CSS/
+│   ├── navbar.css
+│   ├── style.css
+├── Database/
+│   ├── db.sql
+├── JS/
+├── Vendor/
+├── create_user.php
+├── dashboard.php
+├── db.php
+├── edit_form.php
+├── edit_user.php
+├── export.php
+├── form.php
+├── index.php
+├── logout.php
+├── process_form.php
+├── remove_form.php
+├── remove_user.php
+├── success.php
+├── update_form.php
+├── update_user.php
+├── view_forms.php
+└── view_users.php
