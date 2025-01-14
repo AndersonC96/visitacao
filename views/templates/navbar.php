e1202c53454c32032a0d04a4df07cdc1e8a0adb9
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-01-14 13:45:53 -0300

    Update navbar.php

--- views/templates/navbar.php
+++ views/templates/navbar.php
@@ -1,18 +1,18 @@
 <nav class="navbar navbar-expand-lg navbar-custom">
-            <div class="container">
-                <a class="navbar-brand" href="dashboard.php">
-                    <img src="https://static.wixstatic.com/media/fef91e_c3f644e14da442178f706149ae38d838~mv2.png/v1/crop/x_0,y_24,w_436,h_262/fill/w_120,h_71,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/CAPA-03.png" alt="Logo">
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    <div class="container">
+        <a class="navbar-brand" href="../public/dashboard.php">
+            <img src="https://static.wixstatic.com/media/fef91e_c3f644e14da442178f706149ae38d838~mv2.png/v1/crop/x_0,y_24,w_436,h_262/fill/w_120,h_71,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/CAPA-03.png" alt="Logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="dashboard.php"><i class="fas fa-home"></i> Home</a>
+                    <a class="nav-link" href="../public/dashboard.php"><i class="fas fa-home"></i> Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="form.php"><i class="fas fa-edit"></i> Formulário</a>
+                    <a class="nav-link" href="../models/form.php"><i class="fas fa-edit"></i> Formulário</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="view_forms.php"><i class="fas fa-eye"></i> Ver Formulários</a>
@@ -20,7 +20,7 @@
                 <li class="nav-item">
                     <a class="nav-link" href="export.php"><i class="fas fa-file-export"></i> Exportar</a>
                 </li>
-                <?php if($user['is_admin']) { ?>
+                <?php if (isset($user['is_admin']) && $user['is_admin']) { ?>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="adminDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         <i class="fas fa-user-cog"></i> Admin
