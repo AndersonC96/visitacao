470fa6d8a249d2e7b47de6c760805a4ca83100da
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-01-14 13:45:50 -0300

    Update header.php

--- views/templates/header.php
+++ views/templates/header.php
@@ -6,10 +6,10 @@
         <!--<title>Simple Pharma | Painel de Controle</title>-->
         <title><?php echo $title ?? 'Simple Pharma'; ?></title>
         <link rel="icon" href="https://static.wixstatic.com/media/5ede7b_719545c97a084f288b8566db52756425%7Emv2.png/v1/fill/w_180%2Ch_180%2Clg_1%2Cusm_0.66_1.00_0.01/5ede7b_719545c97a084f288b8566db52756425%7Emv2.png" type="image/x-icon" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-        <link rel="stylesheet" href="./CSS/navbar.css">
-        <link rel="stylesheet" href="./CSS/footer.css">
+        <link rel="stylesheet" href="../CSS/navbar.css">
+        <link rel="stylesheet" href="../CSS/footer.css">
     </head>
     <body>
         <main>
