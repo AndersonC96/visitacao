c9b1f06117d8a0b40a40fdec9e5d40ff313643ee
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-01-15 13:37:52 -0300

    Update export.php

--- controllers/export.php
+++ controllers/export.php
@@ -119,59 +119,35 @@
         exit();
     }
 ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Exportar para Excel</title>
-        <link rel="icon" href="https://static.wixstatic.com/media/5ede7b_719545c97a084f288b8566db52756425%7Emv2.png/v1/fill/w_180%2Ch_180%2Clg_1%2Cusm_0.66_1.00_0.01/5ede7b_719545c97a084f288b8566db52756425%7Emv2.png" type="image/x-icon" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-        <link rel="stylesheet" href="./CSS/navbar.css">
-    </head>
-    <body>
-        <nav class="navbar navbar-expand-lg navbar-custom shadow-sm bg-light">
-            <div class="container">
-                <a class="navbar-brand" href="#">
-                    <img src="https://static.wixstatic.com/media/fef91e_c3f644e14da442178f706149ae38d838~mv2.png/v1/crop/x_0,y_24,w_436,h_262/fill/w_120,h_71,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/CAPA-03.png" alt="Logo" style="height: 50px;">
-                </a>
-                <button class="navbar-toggler" type="button" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="dashboard.php"><i class="fas fa-home"></i> Voltar ao Início</a>
-                        </li>
-                    </ul>
+<?php
+    $title = "Formulário"; // Define o título da página
+    include '../views/templates/header.php'; // Inclui o cabeçalho
+    include '../views/templates/navbar.php'; // Inclui a Navbar
+?>
+<div class="container mt-4">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h2 class="text-center">Exportar para Excel</h2>
                 </div>
-            </div>
-        </nav>
-        <div class="container mt-4">
-            <div class="row justify-content-center">
-                <div class="col-md-6">
-                    <div class="card">
-                        <div class="card-header">
-                            <h2 class="text-center">Exportar para Excel</h2>
+                <div class="card-body">
+                    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                        <div class="mb-3">
+                            <label for="data_inicio" class="form-label">Data de Início</label>
+                            <input type="date" class="form-control" id="data_inicio" name="data_inicio">
                         </div>
-                        <div class="card-body">
-                            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-                                <div class="mb-3">
-                                    <label for="data_inicio" class="form-label">Data de Início</label>
-                                    <input type="date" class="form-control" id="data_inicio" name="data_inicio">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="data_fim" class="form-label">Data de Término</label>
-                                    <input type="date" class="form-control" id="data_fim" name="data_fim">
-                                </div>
-                                <button type="submit" class="btn btn-primary">Exportar</button>
-                            </form>
+                        <div class="mb-3">
+                            <label for="data_fim" class="form-label">Data de Término</label>
+                            <input type="date" class="form-control" id="data_fim" name="data_fim">
                         </div>
-                    </div>
+                        <button type="submit" class="btn btn-primary">Exportar</button>
+                    </form>
                 </div>
             </div>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-    </body>
-</html>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+<?php include '../views/templates/footer.php'; // Inclui o rodapé ?>
