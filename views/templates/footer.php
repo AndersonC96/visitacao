0da12ecb1ca9b378be813479dad785f03b65db5f
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-01-20 18:18:43 -0300

    Update footer.php

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,7 +1,13 @@
-        <br>
         </main>
-        <footer>
+        <footer class="bg-dark text-light text-center py-3 mt-auto">
             <p>&copy; <?php echo date('Y'); ?> Simple Pharma. Todos os direitos reservados.</p>
+            <div>
+                <a href="https://www.instagram.com/simplepharma/" target="_blank" class="text-light">
+                    <i class="fab fa-instagram"></i>
+                </a>
+            </div>
         </footer>
+        <!-- Bootstrap JavaScript -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+2k9qkU4e59aYdWGH3InknFNZBv0I" crossorigin="anonymous"></script>
     </body>
 </html>
