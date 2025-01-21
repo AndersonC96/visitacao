53a1cff5e125b3a1e617969efb882ca73330da56
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-01-21 12:17:22 -0300

    Update process_form.php

--- models/process_form.php
+++ models/process_form.php
@@ -1,5 +1,5 @@
 <?php
-include 'db.php';
+include '../config/db.php';
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         $nome = $_POST['nome'];
         $numero_registro = $_POST['numero_registro'];
